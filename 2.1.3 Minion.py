25c8e54411b1845787921ac46400162399fd6c8d
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-02-09 14:10:48 -0500

    Create 2.1.3 Minion.py

---
+++ 2.1.3 Minion.py
@@ -0,0 +1,19 @@
+def drawMinion(minionColor):
+    # Change the background to use the function parameter as the fill.
+    ### Fix Your Code Here ###
+    Rect(0, 0, 400, 400, fill=minionColor)
+
+    # Add code to draw the mouth!
+    ### Place Your Code Here ###
+    Oval(200,300,170,80, fill="black")
+    Oval(200,280,170,80, fill=minionColor)
+    # glasses
+    Rect(0, 100, 400, 60)
+    Circle(130, 130, 60, fill='snow', border='dimGray', borderWidth=15)
+    Circle(270, 130, 60, fill='snow', border='dimGray', borderWidth=15)
+
+    # eyes
+    Circle(130, 130, 25, border='saddleBrown', borderWidth=10)
+    Circle(270, 130, 25, border='saddleBrown', borderWidth=10)
+    Circle(115, 115, 5, fill='white')
+    Circle(255, 115, 5, fill='white')
