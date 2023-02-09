0daaf4679fc7f7c0d59104f836b36434a8b7f869
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-02-09 13:40:18 -0500

    Create 1.2.2 Nine gradients.py

---
+++ 1.2.2 Nine gradients.py
@@ -0,0 +1,18 @@
+# Here is some starter code: the following rectangles are in the right locations
+# for the top row, but they still need gradient fills.
+### Modify Your Code Here ###
+Rect(25, 25, 100, 100, fill=gradient("red","yellow", start="top-left"))
+Rect(150, 25, 100, 100, fill=gradient("red","yellow", start="top"))
+Rect(275, 25, 100, 100, fill=gradient("red","yellow", start="top-right"))
+
+# Here is the first of the three rectangles in the middle row.
+### Modify & Place Your Code Here ###
+Rect(25, 150, 100, 100, fill=gradient("red","yellow", start="left"))
+Rect(150, 150, 100, 100, fill=gradient("red","yellow"))
+Rect(275, 150, 100, 100, fill=gradient("red","yellow", start="right"))
+
+# Here is the first of the three rectangles in the bottom row.
+### Modify & Place Your Code Here ###
+Rect(25, 275, 100, 100, fill=gradient("red","yellow", start="bottom-left"))
+Rect(150, 275, 100, 100, fill=gradient("red","yellow", start="bottom"))
+Rect(275, 275, 100, 100, fill=gradient("red","yellow", start="bottom-right"))
