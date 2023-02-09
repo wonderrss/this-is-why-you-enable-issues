78d89a5f091bfdce8390beb3af26a8a93f26570a
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-02-09 13:39:54 -0500

    Create 1.2.2 Diamond pickaxe.py

---
+++ 1.2.2 Diamond pickaxe.py
@@ -0,0 +1,11 @@
+# Make the handle wood colored.
+### Modify Your Code Here ###
+Rect(125, 125, 75, 75, fill=gradient("peru", "maroon"))
+Rect(200, 200, 75, 75,fill=gradient("peru", "maroon"))
+Rect(275, 275, 75, 75,fill=gradient("peru", "maroon"))
+Rect(50, 50, 40, 40,fill=gradient("peru", "maroon"))
+
+# Give the pickaxe a diamond gradient!
+### Modify Your Code Here ###
+Rect(100, 50, 200, 40, fill=gradient("royalBlue","deepSkyBlue","dodgerBlue", start="top"))
+Rect(50, 100, 40, 200, fill=gradient("royalBlue","deepSkyBlue","dodgerBlue", start="left"))
