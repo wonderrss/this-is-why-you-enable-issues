3aba91febfc3e8921df8775817ae1dec9a196091
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-02-09 13:52:38 -0500

    Create 1.3 Cat.py

---
+++ 1.3 Cat.py
@@ -0,0 +1,26 @@
+# background
+Rect(0, 0, 400, 400, fill='lightGrey')
+
+# head and body
+Circle(190, 220, 100, fill='white')
+Oval(325, 350, 325, 250, fill='white')
+
+# left ear
+Polygon(100, 130, 100, 180, 150, 130, fill='dimGrey')
+
+# Draw the right ear.
+### Place Your Code Here ###
+Polygon(230,130,280,130,280,180, fill="orange")
+# face
+Circle(230, 220, 40, fill='orange')
+Circle(120, 220, 8)
+Circle(220, 220, 8)
+Oval(170, 235, 15, 10, fill='pink')
+
+# Add a collar and wiskers to the cat!
+### Place Your Code Here ###
+Line(165,325,300,230, fill="tomato", lineWidth=16)
+Line(70,250,110,250)
+Line(70,260,110,260)
+Line(240,250,270,250)
+Line(240,260,270,260)
