d372f026d2ab5ec4a6a3a63e7c9ffb0b830d0ff5
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-02-09 16:44:59 -0500

    Create 2.2.2 Eat the cookie.py

---
+++ 2.2.2 Eat the cookie.py
@@ -0,0 +1,28 @@
+# background
+Rect(0, 0, 400, 400, fill='lightSteelBlue')
+
+# cookie
+Circle(205, 205, 155, fill='saddleBrown')
+Circle(200, 200, 150, fill='peru')
+
+def drawChocoChips(centerX, centerY):
+    Circle(centerX, centerY, 10, fill=rgb(90, 35, 15))
+
+# chocolate chips
+drawChocoChips(140, 90)
+drawChocoChips(215, 150)
+drawChocoChips(275, 100)
+drawChocoChips(315, 200)
+drawChocoChips(270, 300)
+drawChocoChips(230, 240)
+drawChocoChips(150, 315)
+drawChocoChips(150, 245)
+drawChocoChips(80, 220)
+drawChocoChips(120, 155)
+drawChocoChips(190, 200)
+
+
+def onMouseRelease(mouseX, mouseY):
+    # Add code to draw a circular bite.
+    ### Place Your Code Here ###
+    Circle(mouseX, mouseY, 40, fill="lightSteelBlue")
