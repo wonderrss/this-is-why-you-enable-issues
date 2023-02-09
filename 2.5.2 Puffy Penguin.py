6eef52660317c9314c568bc52df43ef98e476b3e
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-02-09 16:54:19 -0500

    Create 2.5.2 Puffy Penguin.py

---
+++ 2.5.2 Puffy Penguin.py
@@ -0,0 +1,37 @@
+app.background = gradient('white', 'aliceBlue')
+
+# feet
+Oval(125, 305, 20, 50, fill='orange', rotateAngle=50)
+Oval(135, 310, 20, 50, fill='orange', rotateAngle=20)
+Oval(145, 310, 20, 50, fill='orange', rotateAngle=-10)
+Oval(275, 305, 20, 50, fill='orange', rotateAngle=-50)
+Oval(265, 310, 20, 50, fill='orange', rotateAngle=-20)
+Oval(255, 310, 20, 50, fill='orange', rotateAngle=10)
+
+# wings
+Oval(110, 150, 65, 120, rotateAngle=-40)
+Oval(110, 160, 35, 110, fill='white', rotateAngle=-40)
+Oval(290, 150, 65, 120, rotateAngle=40)
+Oval(290, 160, 35, 110, fill='white', rotateAngle=40)
+
+# belly
+Oval(200, 150, 150, 230)
+belly = Circle(200, 230, 70, fill='white', border='black', borderWidth=15)
+
+# body
+Oval(175, 160, 65, 180, fill='white')
+Oval(225, 160, 65, 180, fill='white')
+Circle(175, 110, 10)
+Circle(225, 110, 10)
+Polygon(185, 130, 215, 130, 205, 150, fill='orange')
+
+# words
+hungryLabel = Label("I'm full!", 320, 50, size=35, visible=False)
+
+def onMousePress(mouseX, mouseY):
+    # Increase the size of belly to make the penguin puffy.
+    ### (HINT: If the belly has a radius of over 100, show hungryLabel.)
+    ### Place Your Code Here ###
+    belly.radius+=3
+    if (belly.radius>100):
+        hungryLabel.visible=True
