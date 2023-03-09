ea7c1de7ebda0cbdb7664d9f441e9ee33444251a
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-03-09 12:22:26 -0500

    Add Checkpoints

---
+++ 1.2.3.1 Rectangles, Ovals, Circles, and Lines Checkpoint 4.py
@@ -0,0 +1,5 @@
+# Draw a 300x300 navy square centered at (200, 200) and a cyan circle
+# centered inside that square with a radius that just fills the square.
+### Place Your Code Here ###
+Rect(50,50,300,300, fill="navy")
+Circle(200,200,150, fill="cyan")
