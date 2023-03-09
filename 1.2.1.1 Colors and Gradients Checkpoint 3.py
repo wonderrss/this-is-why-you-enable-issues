dc234dbd63cda59719e2fb98c27988d44117f158
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-03-09 12:21:09 -0500

    Add Checkpoints

---
+++ 1.2.1.1 Colors and Gradients Checkpoint 3.py
@@ -0,0 +1,4 @@
+# Edit the fill of this rectangle so it matches the red and yellow radial gradient
+# in the solution.
+### Modify Your Code Here ###
+Rect(150, 150, 100, 100, fill=gradient("red","yellow"))
