bd967b26b672d6f9563fe96a570d86329f6607d3
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-03-09 12:20:42 -0500

    Add Checkpoints

---
+++ 1.1.4.1 Fills and Borders Checkpoint 3.py
@@ -0,0 +1,11 @@
+# Draw a rectangle that creates a colorful background!
+### (HINT: Remember to use the inspector to find the color of the rectangle!)
+### (HINT: The corners of the Rect should be the corners of the Canvas.)
+### Place Your Code Here ###
+Rect(0,0,400,400, fill="lightCyan")
+
+# Draw the three other Rects in the correct order to get the solution.
+### Place Your Code Here ###
+Rect(40,20,160,160, fill="dodgerBlue")
+Rect(120,100,160,200, fill="gold")
+Rect(200,220,160,160, fill="dodgerBlue")
