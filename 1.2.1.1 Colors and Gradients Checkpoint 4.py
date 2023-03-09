6bca2c7d941c30c5db98547c8483bc394a503877
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-03-09 12:21:37 -0500

    Add Checkpoints

---
+++ 1.2.1.1 Colors and Gradients Checkpoint 4.py
@@ -0,0 +1,7 @@
+# Edit only the start values of these 4 rectangles so their gradients
+# match the solution.
+### Modify Your Code Here ###
+Rect( 40, 50, 70, 70, fill=gradient('red', 'blue', start='bottom-right'))
+Rect(125, 50, 70, 70, fill=gradient('red', 'blue', start='top'))
+Rect(210, 50, 70, 70, fill=gradient('red', 'blue', start='bottom-left'))
+Rect(295, 50, 70, 70, fill=gradient('red', 'blue', start='right'))
