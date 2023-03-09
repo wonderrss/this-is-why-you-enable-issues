93e6ca64399554d4b4cf0e6ee804a17c03f0ed84
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-03-09 12:24:04 -0500

    Add Checkpoints

---
+++ 1.2.4.1 Ovals, Circles, Lines, and Polygons 3.py
@@ -0,0 +1,8 @@
+# This draws the background.
+Rect(0, 0, 400, 400, fill='lightYellow')
+
+# Draw the two labels. Remember to use the inspector to see what properties
+# are set!
+### Place Your Code Here ###
+Label("Good", 125, 150, size=75, fill="steelBlue", font="arial", italic=True)
+Label("Morning",225,250, size=75, fill="steelBlue", font="arial", bold=True)
