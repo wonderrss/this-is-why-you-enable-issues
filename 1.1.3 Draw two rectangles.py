654834636629fc4f5286a1e4689f8cf07847a29c
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:02:24 +0000

    Delete 1.1.3 Draw two rectangles.py

--- 1.1.3 Draw two rectangles.py
+++
@@ -1,5 +0,0 @@
-# Draw two rectangles to match the solution.
-### (HINT: Use the Inspector to find the left, top, width, and height!)
-### Place Your Code Here ###
-Rect(200, 0, 200, 200)
-Rect(0, 200, 200, 200)
