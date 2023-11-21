0f02751186a6c168ff8b3fd3d19683170f34bdd0
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:03:47 +0000

    Delete 1.2.2 Hazy sun.py

--- 1.2.2 Hazy sun.py
+++
@@ -1,6 +0,0 @@
-# Draw the solution image.
-### (HINT: There are only two rectangles in the solution and
-#          both have gradients!)
-### Place Your Code Here ###
-Rect(0,0,400,300, fill=gradient("yellow","skyBlue", "skyBlue"))
-Rect(0,300,400,100, fill=gradient("lawnGreen","paleGreen", start="bottom"))
