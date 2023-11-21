a18dd67ce3653e489f5c521251d9e1bb2c54f0f3
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:03:11 +0000

    Delete 1.1.5 Flag of Ireland.py

--- 1.1.5 Flag of Ireland.py
+++
@@ -1,7 +0,0 @@
-# Draw the flag of Ireland!
-### (HINT: To draw the border, use Rect with a fill of None and the Inspector to
-#          find the borderWidth.)
-### Place Your Code Here ###
-Rect(50,100,100,200, fill="green")
-Rect(250,100,100,200, fill="orange")
-Rect(50,100,300,200, fill=None, border="Black", borderWidth=5)
