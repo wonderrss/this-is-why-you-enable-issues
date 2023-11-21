af0d38bc9834af6b04d256091fcfd9deb9464d41
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:02:54 +0000

    Delete 1.1.4.1 Fills and Borders Checkpoint 2.py

--- 1.1.4.1 Fills and Borders Checkpoint 2.py
+++
@@ -1,7 +0,0 @@
-# Fix the error in this code so it draws a 100x100
-# brown square centered in the canvas.
-
-### (HINT: Use Rect to draw a square!)
-### (HINT: This has two errors!)
-### Fix Your Code Here ###
-Rect(150, 150, 100, 100, fill="brown")
