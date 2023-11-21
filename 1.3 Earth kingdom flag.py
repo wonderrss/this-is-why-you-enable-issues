3bcca46e1564a52bf94b45321651685eb7c0db41
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:05:34 +0000

    Delete 1.3 Earth kingdom flag.py

--- 1.3 Earth kingdom flag.py
+++
@@ -1,6 +0,0 @@
-# Draw the Earth Kingdom Flag!
-### (HINT: The flag is only made up of 3 shapes!)
-### Place Your Code Here ###
-Rect(20,75,360,250, fill=rgb(100,160,80),border="gold", borderWidth=7)
-Circle(200,200,80, fill=rgb(40,90,40), border="gold", borderWidth=7)
-Rect(170,170,60,60, fill=rgb(100,160,80), border="gold", borderWidth=7)
