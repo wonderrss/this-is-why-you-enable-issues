b442347e2911bde6c83549213c11be8e2ae64a38
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:05:00 +0000

    Delete 1.2.5 Scotty dog.py

--- 1.2.5 Scotty dog.py
+++
@@ -1,8 +0,0 @@
-# Draws the Scotty Dog head.
-Polygon(130, 60, 150, 140, 150, 70, 170, 130, 260, 130, 220, 150, 325, 160,
-        315, 260, 290, 240, 210, 240, 210, 270, 250, 310, 180, 380, 80, 280,
-        130, 205)
-
-# Draw the scarf around the neck.
-### Place Your Code Here ###
-Polygon(80,280,130,205,210,270,250,310, fill="red")
