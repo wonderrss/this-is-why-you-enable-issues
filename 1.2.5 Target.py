f6d05a6c853a710afc5794659938ff8a3799b43e
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:05:11 +0000

    Delete 1.2.5 Target.py

--- 1.2.5 Target.py
+++
@@ -1,8 +0,0 @@
-# draw the target
-Circle(200, 150, 100, fill='red')
-Circle(200, 150, 70, fill='white')
-Circle(200, 150, 35, fill='red')
-
-# You are given the target. Use Label to add the name and complete the logo.
-### Place Your Code Here ###
-Label("TARGET", 200,300, fill="red", font="arial", size=65, bold=True)
