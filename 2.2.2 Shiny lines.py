5ee11998af7c66983b7e39973306d0826c59c7ab
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:08:07 +0000

    Delete 2.2.2 Shiny lines.py

--- 2.2.2 Shiny lines.py
+++
@@ -1,8 +0,0 @@
-# background
-Rect(0, 0, 400, 400)
-
-def onMousePress(mouseX, mouseY):
-    # Draw a line from left-top corner to where the mouse is being clicked.
-    ### (HINT: Don't forget about the gradient!)
-    ### Place Your Code Here ###
-    Line(0,0,mouseX, mouseY, fill=gradient("red","black"), lineWidth=2)
