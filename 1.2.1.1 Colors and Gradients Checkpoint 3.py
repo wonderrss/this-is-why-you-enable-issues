da40e07da30b1b2c7984759d2905cf97093dbfe4
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:03:32 +0000

    Delete 1.2.1.1 Colors and Gradients Checkpoint 3.py

--- 1.2.1.1 Colors and Gradients Checkpoint 3.py
+++
@@ -1,4 +0,0 @@
-# Edit the fill of this rectangle so it matches the red and yellow radial gradient
-# in the solution.
-### Modify Your Code Here ###
-Rect(150, 150, 100, 100, fill=gradient("red","yellow"))
