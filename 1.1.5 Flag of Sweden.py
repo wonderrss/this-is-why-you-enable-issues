d49df0ae6a80e1f1835490c8e80922be8cdaaac8
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:03:24 +0000

    Delete 1.1.5 Flag of Sweden.py

--- 1.1.5 Flag of Sweden.py
+++
@@ -1,5 +0,0 @@
-# Draw the flag of Sweden. Be sure to use the Inspector!
-### Place Your Code Here ###
-Rect(50,100,300,200, fill="steelBlue")
-Rect(50,180,300,40, fill="gold")
-Rect(140,100,40,200, fill="gold")
