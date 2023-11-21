b935a83f095d218efeb44767da88f14edae1e227
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:03:17 +0000

    Delete 1.1.5 Flag of Mali.py

--- 1.1.5 Flag of Mali.py
+++
@@ -1,8 +0,0 @@
-# Draw the flag of Mali.
-### (HINT: To draw the border, use a Rect with a fill of None and the Inspector
-#          to find the borderWidth.)
-### Place Your Code Here ###
-Rect(50,100,100,200, fill="forestGreen")
-Rect(150,100,100,200, fill="gold")
-Rect(250,100,100,200, fill="red")
-Rect(50,100,300,200, fill=None, border="Black", borderWidth=5)
