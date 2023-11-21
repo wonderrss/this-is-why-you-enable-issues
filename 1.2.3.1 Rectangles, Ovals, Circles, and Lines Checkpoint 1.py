d5388ed3daf7e5baa5bf68b0622301f574fc0254
VGmaster670 <72361126+VGmaster670@users.noreply.github.com>
2023-11-21 14:04:09 +0000

    Delete 1.2.3.1 Rectangles, Ovals, Circles, and Lines Checkpoint 1.py

--- 1.2.3.1 Rectangles, Ovals, Circles, and Lines Checkpoint 1.py
+++
@@ -1,5 +0,0 @@
-# Without changing the green rectangle, move the
-# gold oval so it is centered inside the rectangle.
-### Modify Your Code Here ###
-Rect(200, 200, 150, 50, fill='green')
-Oval(275, 225, 150, 50, fill='gold')
